0866e6a9f529c6855520a70a7118bbecb8dfce5e
jcferrei <joao.ferreiradasilva@epfl.ch>
2025-04-24 08:39:31 +0200

    embedding discord

---
+++ docs/js/discord.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    new Crate({
+      server: '1364844683246702632',
+      channel: '1364844687382282344',
+      location: ['bottom-right', 'bottom-right'],
+      color: '#5865F2'
+    })
+  });
